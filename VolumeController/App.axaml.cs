1e18eb21f7df20ab59a9e06ae14262f14c6d1a89
Devvvmn <devvvman@protonmail.com>
2025-05-08 01:14:56 +0300

    set position, visual ed

--- VolumeController/App.axaml.cs
+++ VolumeController/App.axaml.cs
@@ -6,16 +6,24 @@ namespace VolumeController;
 
 public partial class App : Application
 {
-    public override void Initialize()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
+    public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            int x = 0;
+            int y = 0;
+
+            if (desktop.Args.Length >= 2 &&
+                int.TryParse(desktop.Args[0], out var parsedX) &&
+                int.TryParse(desktop.Args[1], out var parsedY))
+            {
+                x = parsedX;
+                y = parsedY;
+            }
+
+            desktop.MainWindow = new MainWindow(x, y);
         }
 
         base.OnFrameworkInitializationCompleted();
--- VolumeController/MainWindow.axaml.cs
+++ VolumeController/MainWindow.axaml.cs
@@ -1,11 +1,59 @@
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
 
-namespace VolumeController;
-
-public partial class MainWindow : Window
+namespace VolumeController
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow(int x, int y)
+        {
+            InitializeComponent();
+            // Получаем ссылку на слайдер
+            var slider = this.FindControl<Slider>("Slider");
+
+            // Подписываемся на событие изменения слайдера
+            slider.ValueChanged += VolumeChanged;
+            
+            Position = new PixelPoint(x, y);
+            
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        // Обработчик события для изменения значения слайдера
+        private void VolumeChanged(object? sender, RangeBaseValueChangedEventArgs e)
+        {
+            var slider = (Slider)sender!;
+            var volume = slider.Value;
+
+            // Убедимся, что значение громкости в пределах допустимого диапазона
+            if (volume < 0) volume = 0;
+            if (volume > 100) volume = 100;
+
+            SetVolume(volume);
+        }
+
+        private void SetVolume(double volume)
+        {
+            var volumeCommand = $"pactl set-sink-volume @DEFAULT_SINK@ {volume}%";
+
+            var processStartInfo = new ProcessStartInfo("zsh", $"-c \"{volumeCommand}\"")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // Запускаем процесс
+            var process = Process.Start(processStartInfo);
+            process?.WaitForExit();
+        }
+        
     }
 }
